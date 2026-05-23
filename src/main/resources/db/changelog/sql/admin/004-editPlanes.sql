bc4b3ba16757f675735f75789579c0eedace7a75
ivanCalderon <ivancalderonburga@gmail.com>
2026-05-23 00:54:56 -0500

    correcion base de datos

---
+++ src/main/resources/db/changelog/sql/admin/004-editPlanes.sql
@@ -0,0 +1,7 @@
+-- liquibase formatted sql
+
+-- changeset alex:004-edit-planes-noop
+-- comment Placeholder migration to keep changelog ordering without breaking startup.
+SELECT 1;
+
+-- rollback SELECT 1;
